64e051851b80697603953e2c127c794080f32018
Liang Zhang <psychelzh@outlook.com>
2023-10-19 12:00:21 +0800

    Support remove suffix (e.g.`PRO`) from task names
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
-replace_as_name_cn <- function(name) {
+replace_as_name_cn <- function(name, remove_suffix = FALSE) {
   parts <- str_split(name, "\\.")
   out <- character(length = length(name))
   for (i in seq_along(name)) {
@@ -7,6 +7,12 @@ replace_as_name_cn <- function(name) {
       select(game_name_abbr, game_name) |>
       deframe() |>
       _[this_parts[[1]]]
+    if (remove_suffix) {
+      this_parts[[1]] <- str_remove(
+        this_parts[[1]],
+        "[a-zA-Z]+$"
+      )
+    }
     out[[i]] <- str_c(this_parts, collapse = ".")
   }
   out
