1da9acff39962229792a9c4ff7a02053c2f34820
Liang Zhang <psychelzh@outlook.com>
2023-10-19 13:31:07 +0800

    Add apply function to `combine_targets()`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/targets.R
+++ R/targets.R
@@ -56,16 +56,28 @@ targets_sample_tasks <- function(num_tasks, data,
   )
 }
 
-combine_targets <- function(name, targets, cols_targets) {
-  name <- deparse1(substitute(name))
+combine_targets <- function(name, targets, cols_targets, fun = NULL) {
+  ischar_name <- tryCatch(
+    is.character(name) && length(name) == 1L,
+    error = function(e) FALSE
+  )
+  if (!ischar_name) {
+    name <- deparse1(substitute(name))
+  }
+  if (is.null(fun)) {
+    fun <- \(x) x
+  }
   tarchetypes::tar_combine_raw(
     name,
     targets[[name]],
-    command = bind_rows(!!!.x, .id = "id") |>
-      # note there is delimiter after name should be removed too
-      mutate(id = str_remove(id, str_c(name, "."))) |>
-      separate(id, cols_targets, convert = TRUE) |>
-      substitute()
+    command = bquote(
+      list(!!!.x) |>
+        lapply(.(rlang::as_function(fun))) |>
+        bind_rows(.id = "id") |>
+        # note there is delimiter after name should be removed too
+        mutate(id = str_remove(id, str_c(.(name), "."))) |>
+        separate(id, .(cols_targets), convert = TRUE)
+    )
   )
 }
 
