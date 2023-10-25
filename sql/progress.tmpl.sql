186ce0260346161a0f825323bcb6af6fda0463d8
Liang Zhang <psychelzh@outlook.com>
2023-10-25 16:04:31 +0800

    Add `project_name`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- sql/progress.tmpl.sql
+++ sql/progress.tmpl.sql
@@ -1,8 +1,10 @@
 SELECT
-  ProjectCourseConfigId project_id,
-  OrganizationUserId user_id,
-  Progress project_progress
+  project_course_user.ProjectCourseConfigId project_id,
+  project_course_config.Name project_name,
+  project_course_user.OrganizationUserId user_id,
+  project_course_user.Progress project_progress
 FROM
   iquizoo_business_db.project_course_user
+  INNER JOIN iquizoo_business_db.project_course_config ON project_course_config.Id = project_course_user.ProjectCourseConfigId
 WHERE
-  ProjectCourseConfigId = ?
+  project_course_config.Id = ?
