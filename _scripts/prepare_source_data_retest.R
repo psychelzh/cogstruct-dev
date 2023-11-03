86aedc0097226dcb53798b5cc7377f17868166bd
Liang Zhang <psychelzh@outlook.com>
2023-11-03 22:21:08 +0800

    Add raw data validation
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/prepare_source_data_retest.R
+++ _scripts/prepare_source_data_retest.R
@@ -8,7 +8,8 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("tarflow.iquizoo"), # packages that your targets need to run
+  packages = c("tarflow.iquizoo", "tidyverse"), # packages that your targets need to run
+  imports = "preproc.iquizoo",
   format = "qs", # Optionally set the default storage format. qs is fast.
   error = "null",
   #
@@ -23,8 +24,33 @@ tar_option_set(
 # Run the R scripts in the R/ folder with your custom functions:
 tar_source()
 
+games_keyboard <- readr::read_lines("config/games_keyboard")
 contents <- tarflow.iquizoo:::fetch_iquizoo_mem(
   readr::read_file("sql/contents_with_retest.sql")
+)
+targets_valid_raw <- tarchetypes::tar_map(
+  values = contents |>
+    dplyr::distinct(game_id) |>
+    dplyr::inner_join(data.iquizoo::game_info, by = "game_id") |>
+    dplyr::mutate(
+      game_id = as.character(game_id),
+      require_keyboard = game_name %in% games_keyboard,
+      tar_parsed = rlang::syms(
+        stringr::str_glue("raw_data_parsed_{game_id}")
+      )
+    ),
+  names = game_id,
+  tar_target(
+    data_valid,
+    validate_data(tar_parsed, require_keyboard)
+  )
+)
+targets_preproc <- tarflow.iquizoo:::tar_action_raw_data(
+  contents |>
+    dplyr::distinct(game_id),
+  name_parsed = "data_valid",
+  action_raw_data = "preproc",
+  add_combine_pre = FALSE
 )
 
 # Replace the target list below with your own:
@@ -32,8 +58,11 @@ list(
   tarflow.iquizoo::tar_prep_iquizoo(
     contents = contents,
     what = "raw_data", # change to "scores" or "raw_data" if you want to
-    action_raw_data = "all",
+    action_raw_data = "parse",
     check_progress = FALSE # set as `FALSE` if projects finalized
-  )
+  ),
   # more targets goes here
+  tar_prep_creativity(),
+  targets_valid_raw,
+  targets_preproc
 )
