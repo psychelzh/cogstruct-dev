4bc8132f91f05d7a01da334eb7cddd1f352df666
Liang Zhang <psychelzh@outlook.com>
2024-01-05 21:55:29 +0800

    Do not remove deleted contents
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- sql/contents_camp.sql
+++ sql/contents_camp.sql
@@ -13,7 +13,7 @@ FROM
     iquizoo_business_db.project_course_config pcc
     INNER JOIN iquizoo_content_db.course c ON c.Id = pcc.CourseId AND c.Deleted <> 1 AND pcc.Deleted <> 1
     INNER JOIN iquizoo_content_db.course_child cc ON cc.CourseId = c.Id AND cc.Deleted <> 1
-    INNER JOIN iquizoo_content_db.course_child_config ccc ON ccc.ChildCourseId = cc.Id AND ccc.Deleted <> 1
+    INNER JOIN iquizoo_content_db.course_child_config ccc ON ccc.ChildCourseId = cc.Id
     INNER JOIN iquizoo_content_db.content c2 ON c2.Id = ccc.ContentId AND c2.ContentType <> 4 AND c2.Deleted <> 1
     INNER JOIN iquizoo_user_db.base_organization bo ON bo.Id = pcc.OrganizationId AND bo.Deleted <> 1
 WHERE
@@ -21,9 +21,9 @@ WHERE
         SELECT org_base.Id
         FROM
             iquizoo_user_db.base_organization org_base
-            INNER JOIN iquizoo_user_db.base_organization org_sup
-                ON org_base.SuperiorId = org_sup.Id AND org_base.Deleted <> 1 AND org_sup.Deleted <> 1
-            WHERE org_sup.Name = 'CAMP'
+        INNER JOIN iquizoo_user_db.base_organization org_sup
+            ON org_base.SuperiorId = org_sup.Id AND org_base.Deleted <> 1 AND org_sup.Deleted <> 1
+        WHERE org_sup.Name = 'CAMP'
     )
     AND c.CourseSeriesId = (
         SELECT cs.Id
