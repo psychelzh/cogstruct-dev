8c32a5e510c919517b0d22f449d77960dc1cc62f
Liang Zhang <psychelzh@outlook.com>
2024-01-05 11:28:16 +0800

    Add `prefix` argument
    Let code be more compact and readable.
    
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/targets.R
+++ R/targets.R
@@ -403,22 +403,17 @@ tar_sample_tasks <- function(num_tasks, data,
 }
 
 # general targets factory ----
-tar_combine_with_meta <- function(name, targets, cols_targets,
-                                  fun_pre = NULL,
-                                  fun_post = NULL) {
+tar_combine_with_meta <- function(name, targets, cols_targets, ...,
+                                  prefix = NULL,
+                                  fun_pre = identity,
+                                  fun_post = identity) {
+  rlang::check_dots_empty()
   ischar_name <- tryCatch(
     is.character(name) && length(name) == 1L,
     error = function(e) FALSE
   )
-  if (!ischar_name) {
-    name <- deparse1(substitute(name))
-  }
-  if (is.null(fun_pre)) {
-    fun_pre <- \(x) x
-  }
-  if (is.null(fun_post)) {
-    fun_post <- \(x) x
-  }
+  if (!ischar_name) name <- deparse1(substitute(name))
+  if (is.null(prefix)) prefix <- name
   tarchetypes::tar_combine_raw(
     name,
     targets,
@@ -426,8 +421,8 @@ tar_combine_with_meta <- function(name, targets, cols_targets,
       list(!!!.x) |>
         lapply(.(rlang::as_function(fun_pre))) |>
         bind_rows(.id = "id") |>
-        # note there is delimiter after name should be removed too
-        mutate(id = str_remove(id, str_c(.(name), "."))) |>
+        # note there is delimiter after prefix should be removed too
+        mutate(id = str_remove(id, str_c(.(prefix), "."))) |>
         separate(id, .(cols_targets), convert = TRUE) |>
         .(rlang::as_function(fun_post))()
     )
