91b04bd0cef8b9abe948c7d51cfdad933681ebcc
Liang Zhang <psychelzh@outlook.com>
2024-01-15 00:19:36 +0800

    Enhance code styles
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/preproc_neural.R
+++ _scripts/preproc_neural.R
@@ -12,23 +12,21 @@ tar_source()
 # these external data paths are specially for current pipeline
 root_bids_xcpd <- r"(F:\CAMP)"
 
-fc_preparation <- tarchetypes::tar_map(
-  tidyr::expand_grid(
-    hypers_xcpd_config,
-    hypers_fmri_dataset,
-    hypers_atlas
-  ),
-  tar_target(
-    files_ts,
-    prepare_files_ts(config, session, task, atlas)
-  ),
-  tar_target(
-    ts_merged,
-    prepare_ts_merged(files_ts)
-  ),
-  tar_target(fc_orig_full, prepare_data_fc(ts_merged))
-)
-
 list(
-  fc_preparation
+  tarchetypes::tar_map(
+    tidyr::expand_grid(
+      hypers_xcpd_config,
+      hypers_fmri_dataset,
+      hypers_atlas
+    ),
+    tar_target(
+      files_ts,
+      prepare_files_ts(config, session, task, atlas)
+    ),
+    tar_target(
+      ts_merged,
+      prepare_ts_merged(files_ts)
+    ),
+    tar_target(fc_orig_full, prepare_data_fc(ts_merged))
+  )
 )
