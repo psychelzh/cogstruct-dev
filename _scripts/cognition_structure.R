2e07a1506891b4ca0eec9bf11188a6589617c6ff
Liang Zhang <psychelzh@outlook.com>
2024-02-23 14:31:48 +0800

    Add extra models based on CHC and cognition theories
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- _scripts/cognition_structure.R
+++ _scripts/cognition_structure.R
@@ -7,6 +7,25 @@ tar_option_set(
 )
 setup_parallel_plan()
 
+targets_cfa <- tarchetypes::tar_map(
+  list(type = rlang::syms(c("efa", "chc", "cognition"))),
+  tar_target(
+    config_dims,
+    config_dims_alternatives |>
+      select(manifest, latent = type)
+  ),
+  tarchetypes::tar_map(
+    hypers_model,
+    tar_fit_cfa(
+      config_dims,
+      indices_splitted[[2]],
+      theory = theory,
+      missing = "pairwise",
+      tar_post_fit = "gof"
+    )
+  )
+)
+
 list(
   tarchetypes::tar_file_read(
     indices_cogstruct,
@@ -21,24 +40,52 @@ list(
     efa_result,
     iterate_efa(indices_splitted[[1]])
   ),
-  tar_target(
-    config_dims,
-    efa_result$efa |>
-      parameters::model_parameters(threshold = "max") |>
-      pivot_longer(
-        starts_with("MR"),
-        names_to = "latent",
-        values_to = "loading",
-        values_drop_na = TRUE
-      ) |>
-      select(latent, manifest = Variable)
+  tarchetypes::tar_file_read(
+    config_dims_theory,
+    "config/game_dims_theory.csv",
+    read = read_csv(!!.x, col_types = cols(game_id = "I"))
   ),
-  tarchetypes::tar_map(
-    hypers_model,
-    tar_fit_cfa(
-      config_dims,
-      indices_splitted[[2]],
-      theory = theory
-    )
+  tar_target(
+    config_dims_alternatives,
+    config_dims_theory |>
+      unite(manifest, game_name_abbr, index_name, sep = ".") |>
+      inner_join(
+        efa_result$efa |>
+          parameters::model_parameters(threshold = "max") |>
+          pivot_longer(
+            starts_with("MR"),
+            names_to = "efa",
+            values_to = "loading",
+            values_drop_na = TRUE
+          ) |>
+          select(efa, manifest = Variable, loading),
+        by = "manifest"
+      )
+  ),
+  tar_target(
+    file_config_dims,
+    config_dims_alternatives |>
+      mutate(variable = match_name_cn_short(manifest)) |>
+      writexl::write_xlsx("_output/config_dims.xlsx"),
+    format = "file"
+  ),
+  targets_cfa,
+  tarchetypes::tar_combine(
+    gofs,
+    zutils::select_list(targets_cfa, starts_with("gof")),
+    command = list(!!!.x) |>
+      map(\(x) as_tibble(x)) |>
+      bind_rows(.id = ".id") |>
+      zutils::separate_wider_dsv(
+        ".id",
+        c(names(hypers_model), "type"),
+        patterns = c(".+?", ".+"),
+        prefix = "gof"
+      )
+  ),
+  tar_fit_cfa(
+    config_dims_efa,
+    indices_cogstruct,
+    theory = "bf"
   )
 )
