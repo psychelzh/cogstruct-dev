518aadb915c520a49d5a88c268213cb29dde99fc
Liang Zhang <psychelzh@outlook.com>
2024-04-18 16:59:09 +0800

    Begin deprecation of old efa functions
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

--- R/deprecated.R
+++ R/deprecated.R
@@ -49,3 +49,83 @@ output_factcons <- function(schema, mat, ...,
   dev.off()
   file
 }
+
+# https://www.open-access.bcu.ac.uk/6076/
+iterate_efa <- function(r, n_obs = 100) { # nolint: cyclocomp_linter.
+  if (!psych::isCorrelation(r)) {
+    n_obs <- nrow(r)
+    r <- cor(r, use = "pairwise.complete.obs")
+  }
+  nfact <- psych::fa.parallel(r, n_obs, plot = FALSE)$nfact
+  efa <- psych::fa(r, nfact, n_obs)
+  removed <- list(
+    loading_too_small = character(),
+    loading_cross = character()
+  )
+  repeat {
+    stable <- TRUE
+    # ensure all factors have at least 3 loadings above 0.4
+    repeat {
+      loadings <- loadings(efa)
+      if (sum(apply(loadings, 2, function(x) sum(x > 0.4)) < 3) == 0) {
+        break
+      }
+      stable <- FALSE
+      nfact <- nfact - 1
+      efa <- psych::fa(r, nfact, n_obs)
+    }
+    # ensure items with no loading above 0.3 are removed
+    repeat {
+      loadings <- loadings(efa)
+      loading_too_small <- apply(loadings, 1, max) < 0.3
+      if (!any(loading_too_small)) {
+        break
+      }
+      stable <- FALSE
+      removed$loading_too_small <- c(
+        removed$loading_too_small,
+        names(which(loading_too_small))
+      )
+      r <- r[!loading_too_small, !loading_too_small]
+      efa <- psych::fa(r, nfact, n_obs)
+    }
+    # ensure no cross-loading
+    repeat {
+      loadings <- loadings(efa)
+      loading_cross <- apply(loadings, 1, check_cross_loading)
+      if (!any(loading_cross)) {
+        break
+      }
+      stable <- FALSE
+      removed$loading_cross <- c(
+        removed$loading_cross,
+        names(which(loading_cross))
+      )
+      r <- r[!loading_cross, !loading_cross]
+      efa <- psych::fa(r, nfact, n_obs)
+    }
+    if (stable) {
+      break
+    }
+  }
+  list(
+    n_fact = nfact,
+    efa = efa,
+    vars = colnames(r),
+    removed = removed
+  )
+}
+
+# helper functions ----
+check_cross_loading <- function(x) {
+  # also check negative cross-loading
+  x <- abs(x)
+  x_cross <- x[x > 0.3]
+  if (length(x_cross) < 2) {
+    return(FALSE)
+  }
+  if (all(x_cross < 0.4) || min(x_cross) > 0.75 * max(x_cross)) {
+    return(TRUE)
+  }
+  return(FALSE)
+}
